9e2c086ffe918e3f097566983d8abe7da0d431db
LaShelle Rauseo <lrauseo3@gmail.com>
2022-12-21 09:48:05 -0600

    changed controllers

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3001
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
